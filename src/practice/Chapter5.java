77febf7816860c2e0c57f721457d4e7d03bf93b1
SenaKanetaka <177509930+Jungkoon@users.noreply.github.com>
2024-09-17 17:43:29 +0900

    課題再提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -1,22 +1,20 @@
 package practice;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Chapter5 {
 
     public static void main(String[] args) {
-        List<Task> tasks = new ArrayList<>();
+        Map<LocalDate, String> tasks = new TreeMap<>();
         
-        tasks.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う"));
-        tasks.add(new Task(LocalDate.of(2021, 9, 15), "○○社面談"));
-        tasks.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う"));
-        tasks.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く"));
-        tasks.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く"));
+        tasks.put(LocalDate.of(2021, 10, 21), "牛乳を買う");
+        tasks.put(LocalDate.of(2021, 9, 15), "○○社面談");
+        tasks.put(LocalDate.of(2021, 12, 4), "手帳を買う");
+        tasks.put(LocalDate.of(2021, 8, 10), "散髪に行く");
+        tasks.put(LocalDate.of(2021, 11, 9), "スクールの課題を解く");
         
-        for (Task task : tasks) {
-            System.out.println(task);
-        }
+        tasks.forEach((date, task) -> System.out.println(date + ":" + task));
     }
 }
